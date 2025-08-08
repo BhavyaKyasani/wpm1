5af33b9c0cd1fa58cfb81cd72bd19c1bbed1de8f
BhavyaKyasani <kyasanibhavya23@gmail.com>
2025-08-08 09:21:58 +0530

    first commit

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_Server/routes/index');
-var usersRouter = require('./app_Server/routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_Server', 'views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
